9a1b9556fb188d80cd75047aa41fbe1fb4fb270e
Igwuoku Christabel <71432850+girlbehindthescreen@users.noreply.github.com>
2023-08-24 20:13:29 +0100

    Add files via upload

--- 0x06-unittests_in_js/api.js
+++ 0x06-unittests_in_js/api.js
@@ -3,30 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 7865;
 
-app.use(express.json());
-
-app.get('/', (_req, res) => {
+app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
-});
-
-app.get('/cart/:id(\\d+)', (req, res) => {
-  const id = req.params.id;
-
-  res.send(`Payment methods for cart ${id}`);
-});
-
-app.get('/available_payments', (_req, res) => {
-  res.json({ payment_methods: { credit_cards: true, paypal: false } });
-});
-
-app.post('/login', (req, res) => {
-  let username = '';
-
-  if (req.body) {
-    username = req.body.userName;
-  }
-
-  res.send(`Welcome ${username}`);
 });
 
 app.listen(PORT, () => {
